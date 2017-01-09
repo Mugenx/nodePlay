9ebe6ff3d4e6e23d7294f279eed5b1a00a9453f7
Mugenx <sahawu.mail@gmail.com>
2017-01-08 21:19:29 -0500

    clear

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 /**
  * Created by saha on 2017-01-08.
  */
-console.log("hello world");
-console.log("hello world again");
-console.log("hello world again");
-console.log("hello world again");
-console.log("hello world last");
-console.log("hello world Im back");
+
